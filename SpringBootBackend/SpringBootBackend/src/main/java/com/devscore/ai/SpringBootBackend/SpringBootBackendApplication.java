1031401283f738fdba3f82194cfe862a080f4ca9
Vinamra-tech <vinamrat4@gmail.com>
2026-02-07 14:32:36 +0530

    some task

--- SpringBootBackend/SpringBootBackend/src/main/java/com/devscore/ai/SpringBootBackend/SpringBootBackendApplication.java
+++ SpringBootBackend/SpringBootBackend/src/main/java/com/devscore/ai/SpringBootBackend/SpringBootBackendApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean; // <--- Import this
 import org.springframework.web.reactive.function.client.WebClient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 @SpringBootApplication
 public class SpringBootBackendApplication {
 
