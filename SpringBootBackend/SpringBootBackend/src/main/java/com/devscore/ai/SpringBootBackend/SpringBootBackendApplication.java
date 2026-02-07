8f331f45f367953260c32d5b69d6de0186fe407e
Vinamra-tech <vinamrat4@gmail.com>
2026-02-07 14:20:39 +0530

    WebFlux fix

--- SpringBootBackend/SpringBootBackend/src/main/java/com/devscore/ai/SpringBootBackend/SpringBootBackendApplication.java
+++ SpringBootBackend/SpringBootBackend/src/main/java/com/devscore/ai/SpringBootBackend/SpringBootBackendApplication.java
@@ -2,12 +2,18 @@ package com.devscore.ai.SpringBootBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean; // <--- Import this
+import org.springframework.web.reactive.function.client.WebClient;
 @SpringBootApplication
 public class SpringBootBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBackendApplication.class, args);
 	}
+
+	@Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
 
 }
